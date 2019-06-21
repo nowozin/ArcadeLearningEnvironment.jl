f7e4de83e02949c181ef4acdec8218cd374ef75d
Kartikey Gupta <kartikeygupta9097@gmail.com>
2019-06-22 00:55:22 +0530

    Restructed the RAM functions and modified exports

--- src/aleinterface.jl
+++ src/aleinterface.jl
@@ -177,6 +177,19 @@ getScreen!(ale::ALEPtr, screen_data::Vector{Cuchar}) =
 Returns the size of the RAM
 """
 getRAMSize(ale::ALEPtr) = ccall((:getRAMSize, libale_c), Cint, (ALEPtr,), ale)
+_getRAM(ale::ALEPtr, ram::Vector{Cuchar}) =
+    ccall((:getRAM, libale_c), Cvoid, (ALEPtr, Ptr{Cuchar}), ale, ram)
+
+"""
+Returns the snapshot of the RAM as a vector.
+"""
+function getRAM(ale::ALEPtr)
+    output = Array{UInt8, 1}(undef, getRAMSize(ale))
+    _getRAM(ale, output)
+    return output
+end
+
+# Screen function utilities
 getScreenWidth(ale::ALEPtr) =
     ccall((:getScreenWidth, libale_c), Cint, (ALEPtr,), ale)
 getScreenHeight(ale::ALEPtr) =
@@ -279,10 +292,8 @@ export
     game_over,
     reset_game,
     getLegalActionSet,
-    getLegalActionSet!,
     getLegalActionSize,
     getMinimalActionSet,
-    getMinimalActionSet!,
     getMinimalActionSize,
     getFrameNumber,
     lives,
