9069246692211fab385cf6065daff26baa545d24
Kartikey Gupta <kartikeygupta9097@gmail.com>
2019-06-22 21:42:48 +0530

    Moved exports to ArcadeLearningEnvironment.jl

--- src/ArcadeLearningEnvironment.jl
+++ src/ArcadeLearningEnvironment.jl
@@ -5,5 +5,49 @@ using Compat
 import Compat: @error
 include("aleinterface.jl")
 
+export
+    # Types
+    ALEInterface,
+    ALEPtr,
+    # Functions
+    ALE_new,
+    ALE_del,
+    getInt,
+    getBool,
+    getFloat,
+    setString,
+    setInt,
+    setBool,
+    setFloat,
+    loadROM,
+    act,
+    game_over,
+    reset_game,
+    getLegalActionSet,
+    getLegalActionSize,
+    getMinimalActionSet,
+    getMinimalActionSize,
+    getFrameNumber,
+    lives,
+    getEpisodeFrameNumber,
+    getScreen,
+    getRAM,
+    getRAMSize,
+    getScreenWidth,
+    getScreenHeight,
+    getScreenRGB,
+    getScreenGrayscale,
+    saveState,
+    loadState,
+    cloneState,
+    restoreState,
+    cloneSystemState,
+    restoreSystemState,
+    deleteState,
+    saveScreenPNG,
+    encodeState,
+    encodeStateLen,
+    decodeState,
+    setLoggerMode!
+    getROMList
 end
-
